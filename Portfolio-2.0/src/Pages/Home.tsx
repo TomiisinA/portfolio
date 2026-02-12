2b738d62851bf70ec5a9eb464a581ea2f4d923fe
OLuwatomisin Ajayi <tomisinajayi@gmail.com>
2026-02-12 11:34:34 +0100

    home and not found

---
+++ Portfolio-2.0/src/Pages/Home.tsx
@@ -0,0 +1,13 @@
+
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen ">Home
+
+        {/* Toggle */}
+
+        {/* Navbar */}
+        {/* Main content */}
+    </div>
+  )
+}
